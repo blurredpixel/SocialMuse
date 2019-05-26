c2cda7bf59bd33cf990b5f03b963b4f2138a7f6c
David Fries <dfries4@gmail.com>
2019-05-25 21:57:14 -0400

    error catching if speaker and/or mic doesn't exist

--- poc.py
+++ poc.py
@@ -3,12 +3,16 @@ import socket
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     print('created socket')
-
-    default_speaker = sc.default_speaker()
+    try:
+        default_speaker = sc.default_speaker()
+    except:
+        print('Error in speaker lookup')
     mics = sc.all_microphones(include_loopback=True)
     print(mics[0])
-
-    loopback = sc.get_microphone('Audio',include_loopback=True)
+    try:
+        loopback = sc.get_microphone('Audio',include_loopback=True)
+    except:
+        print('error in microphone binding')
     s.connect(('127.0.0.1',8001))
     with loopback.recorder(samplerate=48000) as mic, default_speaker.player(samplerate=48000) as sp:
         while True:
